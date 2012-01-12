aeba4d939f51e9d406e11d8d30ec9308135c09cc
Inez Korczynski <korczynski@gmail.com>
2012-01-12 22:40:57 +0100

    Oh, missing files!

---
+++ src/polandemcs/PolandEMCSServlet.java
@@ -0,0 +1,14 @@
+package polandemcs;
+
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class PolandEMCSServlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+	}
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+	}
+
+}
